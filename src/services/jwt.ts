912b03564cc36dc83c994d41dfbfdbea22055b4d
Lucas Matos <lucaslmatos18@gmail.com>
2023-07-12 18:54:04 -0300

    REQ 04 - Criado testes - 100% cobertura

--- src/services/jwt.ts
+++ src/services/jwt.ts
@@ -1,15 +1,17 @@
 import jwt, { JwtPayload } from 'jsonwebtoken';
 
+const secret = process.env.JWT_SECRET || 'secret';
+
 export type Payload = {
   id: number,
   username: string,
 };
 
-const sign = (payload: any): JwtPayload | string => jwt.sign(payload, 'secret');
+const sign = (payload: any): JwtPayload | string => jwt.sign(payload, secret);
 
-// const verify = (token: string): JwtPayload | string => jwt.verify(token, 'secret');
+const verify = (token: string): JwtPayload | string => jwt.verify(token, secret);
 
 export default {
   sign,
-  // verify,
+  verify,
 };
